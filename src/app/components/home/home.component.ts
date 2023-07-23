c7d62ce47bdccf1eb28aeed28ec7df0a69e6a9f3
Mehul <mehulshrivastava1011@gmail.com>
2023-07-23 14:47:59 +0530

    Added feature for user profile to have their blog entries in paginated format

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Observable } from 'rxjs';
+import { BlogEntriesPagable } from 'src/app/models/blog-entry.interface';
+import { BlogService } from 'src/app/services/blog-service/blog.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  blogEntries$: Observable<BlogEntriesPagable> = this.blogService.indexAll(
+    1,
+    10
+  );
+  constructor(private blogService: BlogService) {}
+  onPaginateChange(event: PageEvent) {
+    this.blogEntries$ = this.blogService.indexAll(
+      event.pageIndex,
+      event.pageSize
+    );
+  }
 }
