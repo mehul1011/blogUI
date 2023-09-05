ee3cda6251b6d279fe2f9d6c24e1c1d1f404f004
Mehul <mehulshrivastava1011@gmail.com>
2023-09-05 18:21:54 +0530

    Implementation: Added responsiveness behaviour for while portfolio monorepo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { AuthenticationService } from './services/authentication-service/authentication.service';
 import { ScrollService } from './services/scrollService/scroll-service';
@@ -10,11 +10,13 @@ import { ScrollService } from './services/scrollService/scroll-service';
 })
 export class AppComponent {
   title = '<MehulCodes />';
+  isSmallScreen = false;
+  showToggleDropDownMenu = false;
   constructor(
     private router: Router,
     private authService: AuthenticationService,
     private scrollService: ScrollService
-  ) {}
+  ) { }
 
   navigateTo(value: any) {
     this.router.navigate(['../', value]);
@@ -30,5 +32,28 @@ export class AppComponent {
     //   fragment: id
     // };
     // this.router.navigate(['/'], extras);
+  }
+
+  setIsSmallScreen() {
+    // this.isSmallScreen = true;
+    this.showToggleDropDownMenu = !this.showToggleDropDownMenu;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleDocumentClick(event: MouseEvent) {
+    // Check if the click event occurred outside the target element
+    if (!this.isClickInsideComponent(event)) {
+      this.setIsSmallScreen();
+    }
+  }
+
+  // Helper function to check if the click occurred inside the component
+  private isClickInsideComponent(event: MouseEvent): boolean {
+    const buttonElement = document.getElementById("portfolioSmallScreenButton");
+    // const element = event.target as HTMLElement;
+    if (buttonElement) {
+      return buttonElement.contains(event.target as Node);
+    }
+    return false;
   }
 }
